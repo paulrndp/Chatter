b117700ee11d18d36c3cdb478ff13feab66b9245
paulrndp <paulr@DESKTOP-47REJSO>
2022-09-12 21:16:04 +0800

    Add project files.

---
+++ Chatter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chatter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
